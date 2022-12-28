58394e58c16f38986de4292a3e1d75a937df1a5d
LLBonadie <eufacopublicidade@gmail.com>
2022-12-28 16:41:13 +0000

    novo deploy

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home(){
     return (
         <div>
-            <h1>My First API</h1>
+            <h1>My First API Deployed</h1>
             <Contador />
         
         </div>
