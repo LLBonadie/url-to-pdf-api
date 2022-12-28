19091c6db9b4be32b68a1c3ba848bba130ad89d0
LLBonadie <eufacopublicidade@gmail.com>
2022-12-28 17:31:42 +0000

    test calc

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,15 @@
 function tempo(request, resposnse){
 
+    function math(a, b){
+        return a + b
+    };
+
+    cont = resultCalc(10, 5)
+
     const dynamicDate = new Date();
     resposnse.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        calc: resultCalc
     })
 
 }
